3f63c854f6c2fa45113628aa45632e635279a569
JARODZ <javiro3004@gmail.com>
2025-02-23 07:54:37 -0500

    variables y operadores aritmeticos

---
+++ index.js
@@ -0,0 +1,21 @@
+//operacioones aritmeticas 
+console.log(73+1);
+console.log(73+1+112+41+32+89);
+console.log((4+6+9)/77);
+
+// variables 
+
+let a = 10;
+console.log(a);
+a=73;
+
+let b = 7*a;
+
+// secuencia 
+
+let max = 57
+let actual = max - 13
+let porcentaje = actual / max 
+
+console .log (porcentaje)
+
