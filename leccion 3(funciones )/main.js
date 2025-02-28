c88efcea533741b7e06e9838df41aa2251d8629f
JARODZ <javiro3004@gmail.com>
2025-02-28 16:22:27 -0500

    leccion 3 (funciones)

---
+++ leccion 3(funciones )/main.js
@@ -0,0 +1,57 @@
+// funcio para sumar 
+
+/*function sum(num1, num2 ){
+    return num1 + num2;
+}
+
+console.log((sum(1,3)));
+
+// sunncio de animales favoritos 
+
+function animals (animal){
+    return `mi animal favorito es ${animal}`;
+    
+}
+
+console.log(animals("buho"))*/
+
+// ejercicios 
+
+// 1. Escriba una función llamada add7vque tome un número y devuelva ese número + 7.
+
+/*function add7(num){
+    return num + 7 ;
+}
+
+console.log(add7(3));*/
+
+// Escriba una función llamada multiplyque tome 2 números y devuelva su producto 
+
+/*function multiplyque(num1, num2){
+    return num1 * num2;
+}
+
+console.log(multiplyque(3, 4 )); */
+
+//Escriba una función llamada capitalizeque tome una cadena y la devuelva con solo la primera letra en mayúscula. Asegúrese de que pueda aceptar cadenas en minúscula, MAYÚSCULAS o ambas.
+/*function mayus (convertToMayus) 
+
+{
+    let convertToMinus = convertToMayus.toLowerCase();
+
+    return (
+        convertToMinus[0].toUpperCase() + convertToMinus.slice(1)
+    );
+}
+
+console.log(mayus('hola'));*/
+
+// Escriba una función llamada lastLetter que tome una cadena y devuelva la última letra de esa cadena:
+
+/*function lastLetter (cadena ) {
+    let convertToNumber = cadena.length;
+    
+    return cadena.charAt(convertToNumber - 1 );
+}
+
+console.log(lastLetter("como andan"));*/
