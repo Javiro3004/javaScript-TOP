cf02ae32eb7ea7febad9fb75322ae0a265a41c4f
JARODZ <javiro3004@gmail.com>
2025-03-09 14:25:41 -0500

    primera vesion de arrays y loops (for of  y for )

---
+++ leccion 4 ( arrays y loops )/loops.js
@@ -0,0 +1,15 @@
+const cats = ["salvage ", "domestico", "egipcio", "persa" ]
+// for of para arrays 
+for (let cat of cats ) {
+    console.log(cat);
+}
+
+// for para iteracion sin array 
+function calculate (constNumber){
+    for (let number = 0; number <= 10 ; number++) {
+    const  result  = `${constNumber} * ${number} = ${constNumber * number}`;
+    console.log(result)
+    }
+}
+
+calculate(10)
