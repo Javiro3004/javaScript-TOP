03a18d3f7f63aaa265e4880b90252f26f4bfc324
JARODZ <javiro3004@gmail.com>
2025-03-15 11:48:46 -0500

    ultimos ejercicios de leccion 4

--- leccion 4 ( arrays y loops )/for and for of .js
+++ leccion 4 ( arrays y loops )/for and for of .js
@@ -16,3 +16,31 @@ calculate(99)
 */
 
 // cuanta regreiva de 10 a 0 
+/*for (let i =10; i >= 0 ; i--){
+    if(i == 10 ){
+        console.log(`cuenta regresiva ${i}`)
+    }else if (i == 0){
+        console.log(`${i} Despegue !`)
+    }else{
+        console.log(`${i}`)
+    }
+}*/
+
+// rechazar a phil y lola
+const todosPeronas  = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+
+const listaNueva = [];
+
+const listaRechazados = [];
+
+for (const nuevo of todosPeronas){
+    if (nuevo == "Phil" || nuevo == "Lola"){
+        console.log("rechazados")
+        listaRechazados.push(nuevo)
+    }else {
+        console.log("Admitido")
+        listaNueva.push(nuevo)
+    }
+}
+console.log(listaNueva);
+console.log(listaRechazados);
--- leccion 4 ( arrays y loops )/while and do while.js
+++ leccion 4 ( arrays y loops )/while and do while.js
@@ -1,7 +1,10 @@
 // ejemplo 1 
-let contador = 0 ; 
+// let contador = 0 ; 
 
-while (contador <=10){
+/*while (contador <=10){
     console.log(`su numero es ${contador}`);
     contador ++
-}
+}*/
+
+
+
