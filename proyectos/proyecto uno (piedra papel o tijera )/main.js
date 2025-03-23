483eac15a6a73f944123fda4081f7a39b747d034
JARODZ <javiro3004@gmail.com>
2025-03-22 21:58:44 -0500

    nuevo piedra papel o tijera con ui

--- proyectos/proyecto uno (piedra papel o tijera )/main.js
+++ proyectos/proyecto uno (piedra papel o tijera )/main.js
@@ -2,50 +2,64 @@
 
 let humanScore = 0;
 let compucore = 0;
+//variables de DOM
 
+
+
+
+let humanChoice ; 
 // eleccion humana
 function play () {
-function humanChoice (){
-    let userChoice = prompt("escribe 0 para piedra 1 para papel o 2 para tijera " );
 
-    
-    switch (userChoice) {
-        case "0" :
-            return ("piedra");
-            break;
-
-        case "1" :
-            return ("papel");
-            break;
-
-        case "2" :
-            return ("tijera");
-            break
+    function resetDOM (){
+        document.querySelector('.score').innerHTML = '';
+        humanScore = 0;
+        compucore =0 ;
     }
-}
 
-// funcion de elecccion de la computadora
+    resetDOM();
+
+    function humanElection (){
+        const buttons = document.querySelectorAll(".rock, .scissors, .paper");
+
+        buttons.forEach(button =>{
+            button.addEventListener ("click", (e) =>{
+                if (e.target.classList.contains("rock")){
+                    humanChoice="piedra";
+                    console.log("elejio piedra");
+                    onePlay();
+                }else if  (e.target.classList.contains("paper")){
+                    humanChoice="papel";
+                    console.log("elejio papel");
+                    onePlay();
+                } else if (e.target.classList.contains("scissors")){
+                    humanChoice= "tijeras";
+                    console.log("elejio tijeras");
+                    onePlay();
+                }
+            })
+        })
+    }
+
+humanElection();
+
 function getComputerChoice (){
     let randomNumber = Math.floor(Math.random() *3);
 
     switch (randomNumber) {
         case 0 :
             return ("piedra");
-            break;
 
         case 1 :
             return ("papel");
-            break;
 
         case 2 :
-            return ("tijera");
-            break
+            return ("tijeras");
 
 
     }
 } 
 
-let human = humanChoice();
 let computer = getComputerChoice();
 
 
@@ -53,51 +67,70 @@ let computer = getComputerChoice();
 
 
 function onePlay ( ) {
-    console.log(`Humano: ${human}, Computadora: ${computer}`); // Mostrar elecciones
 
-    if ((human == "papel" && computer == "papel"  )||(human == "piedra" && computer == "piedra" ) || (human == "tijera" && computer == "tijera" )){
-        console.log(`Human score: ${humanScore}, Computer score: ${compucore}`);
-        console.log( "Empate"); // Devolver mensaje de empate
-    } else if ((human === "piedra" && computer === "tijeras" ) || (human === "papel" && computer === "piedra") ||(human === "tijera" && computer === "papel")) {
+    const div = document.querySelector(".score")
+
+    const h1Div = document.createElement("h1")
+    h1Div.textContent = `Humano: ${humanChoice}, Computadora: ${computer}`; // Mostrar elecciones
+    div.appendChild(h1Div);
+
+    if ((humanChoice== "papel" && computer == "papel"  )||(humanChoice == "piedra" && computer == "piedra" ) || (humanChoice== "tijeras" && computer == "tijeras" )){
+        const scoreDiv = document.createElement("h3")
+        const loseDiv= document.createElement("p")
+        scoreDiv.textContent= `human score: ${humanScore}, Computer score: ${compucore}`;
+        loseDiv.textContent= "Empate"; // Devolver mensaje de empate
+        div.appendChild(loseDiv)
+        div.appendChild(scoreDiv)
+
+    } else if ((humanChoice=== "piedra" && computer === "tijeras" ) || (humanChoice=== "papel" && computer === "piedra") ||(humanChoice=== "tijeras" && computer === "papel")) {
         humanScore++; // Incrementar puntaje del humano
-        console.log(`Human score: ${humanScore}, Computer score: ${compucore}`);
-        console.log( "Ganaste"); // Devolver mensaje de victoria
+        const scoreDiv = document.createElement("h3")
+        const winDiv= document.createElement("p")
+        scoreDiv.textContent= `human score: ${humanScore}, Computer score: ${compucore}`;
+        winDiv.textContent= "Ganaste"; // Devolver mensaje de victoria
+        div.appendChild(winDiv)
+        div.appendChild(scoreDiv)
     } else {
         compucore++; // Incrementar puntaje de la computadora
-        console.log(`Human score: ${humanScore}, Computer score: ${compucore}`);
-        console.log("Perdiste"); // Devolver mensaje de derrota
+        const scoreDiv = document.createElement("h3")
+        const loseDiv= document.createElement("p")
+        scoreDiv.textContent= `human score: ${humanScore}, Computer score: ${compucore}`;
+        loseDiv.textContent= "Perdiste"; // Devolver mensaje de derrota
+        div.appendChild(loseDiv)
+        div.appendChild(scoreDiv)
+    }
+
+    if(humanScore === 5){
+        const win = document.createElement("h1")
+        win.textContent="ganaste"
+        document.body.appendChild(win);
+        playAgain();
+        resetDOM();
+    }else if (compucore === 5 ) {
+        const lose = document.createElement("h1")
+        lose.textContent="perdiste"
+        document.body.appendChild(lose);
+        playAgain();
+        resetDOM();
     }
 }
 
-onePlay();
-
-function playAgain () {
-    let question = prompt("quieres jugar de nuevo ? ")
-    switch (question) {
-        case "si" :
-            return play(); 
-            break;
-        case "no " :
-            return console.log(fin)
-    }
 }
-playAgain()
-}
-
 play()
 
 function playAgain () {
-    let question = prompt("quieres jugar de nuevo ? ")
+    let question = prompt("volver a jugar ?")
     switch (question) {
         case "si" :
             return play(); 
-            break;
+            
         case "no " :
             return console.log(fin)
     }
 }
 
-playAgain();
+
+
 
 
 
